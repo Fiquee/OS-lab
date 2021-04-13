741d919f44b764d65513bf8874ed1c66023edfaf
afiqirfan00 <56692050+afiqirfan00@users.noreply.github.com>
2021-04-13 17:54:10 +0800

    edited

---
+++ OSLab3/RoundRobinCode.py
@@ -0,0 +1,92 @@
+from queue import Queue
+
+
+def swap(job_list, i, j):
+    for idx in range(0, 6):
+        job_list[idx][i], job_list[idx][j] = job_list[idx][j], job_list[idx][i]
+
+
+def sort_arrival_times(job_list):
+    for i in range(len(job_list[0])):
+        for j in range(i):
+            if job_list[2][i] < job_list[2][j] :
+                print("swap",job_list[2][i],"with",job_list[2][j])
+                swap(job_list, i, j)
+    print("sorted list")            
+    print_time()            
+
+def print_time():
+    print("Job Number\tBurst Time\tArrival Time\tCompletion Time\tWaiting Time\tTurnaround Time")
+    for a in range(job_number):
+        for b in range(0, 6):
+            print(array[b][a], end="\t\t")
+    print()
+
+
+def round_robin():
+    burst_time = array[1].copy()
+    not_yet_queue = array[0].copy()
+    time_quantum = 4  # Suppose its 3 unit
+    t = 0
+    q = Queue()
+
+    while burst_time != [0]*job_number:
+        print()
+        for i in range(job_number):
+            # Check arrival time
+            if (t+time_quantum >= array[2][i] and array[0][i] in not_yet_queue):
+                print("put job", array[0][i], "into the queue")
+                q.put(i)
+                not_yet_queue.remove(array[0][i])
+        print()        
+        index = q.get()
+        jobnum= array[0][index]
+        print("now doing job",jobnum)
+        check=True
+        while check==True:
+            print("now time is at ",t)
+            if  t>=array[2][index] and burst_time[index] > time_quantum:
+                print("Job",jobnum,"has burst time",burst_time[index],"bigger than", time_quantum)
+                t += time_quantum
+                print("now time is at",t)
+                burst_time[index] -= time_quantum
+                print("Job",jobnum,"has burst time",burst_time[index],"left")
+                q.put(index)
+                print("reinsert index",index,"into queue")
+                print()
+                check=False
+            elif t>=array[2][index] and burst_time[index]<=time_quantum:
+                print("Job",jobnum,"has burst time",burst_time[index],"is smaller or equal to", time_quantum)
+                t += burst_time[index]
+                print("now time is at",t)
+                burst_time[index] = 0
+                array[3][index] = t  # Completion time
+                print("job",jobnum,"is finished at time,",t)
+                array[5][index] = t - array[2][index]  # Turnaround time
+                print("job",jobnum,"have turnaround time",array[5][index])
+                array[4][index] = array[5][index] - array[1][index]  # Waiting time
+                print("job",jobnum,"have waiting time",array[4][index])
+                print("job",array[0][index],"| CT :", array[3][index],"TAT :",array[5][index],"WT :",array[4][index])
+                check=False
+            else:
+                t+=1
+
+
+# job_number = int(input("Enter number of jobs: "))
+job_number=4
+array = [[0 for j in range(job_number)]
+         for i in range(6)]  # Initialize 2D Array
+array[0] = [number for number in range(1, job_number+1)]
+array[1] = [9, 5, 3, 5] #burst time
+array[2] = [0, 2, 2, 3] #arrival time 
+
+# array[1] = [int(time) for time in input("Enter burst time: ").split()]
+# array[2] = [int(time) for time in input("Enter arrival time: ").split()]
+print_time()
+sort_arrival_times(array)
+round_robin()
+print()
+print("Final Result: ")
+print_time()
+print("Average waiting time = ", (sum(array[4])/job_number), end="s\n")
+print("Average turnaround time = ", (sum(array[5])/job_number), end="s\n")
--- OSLab4/banker.py
+++ OSLab4/banker.py
@@ -1,52 +1,79 @@
-def findneeded(processIndex, maximum, allocation, needed):
-    k = len(maximum[processIndex])
-    for i in range(0, k):
-        value = maximum[processIndex][i] - allocation[processIndex][i]
-        needed[processIndex][i] = value
+'''
+Write a program to simulate the Banker’s algorithm for the purpose of deadlock avoidance. Your program receives as input the number of processes in the systems and how many devices each job requires to complete execution. Your program shows how devices are allocated to each process as it executes and if the system is currently in a safe or unsafe state.
+'''
 
 
-def compare(available, needed, allocation, index):
-    for i in range(len(allocation[index])):
-        if(needed[index][i] > available[i]):
-            return False
-    return True
+def calculate_needed(n, m, max_resources, allocated_resources):
+    needed_resources = [[0 for i in range(m)] for j in range(n)]
+    for i in range(n):
+        for j in range(m):
+            needed_resources[i][j] = max_resources[i][j] - \
+                allocated_resources[i][j]
+
+    return needed_resources
 
 
-def addAvailable(available, allocation, index):
-    for i in range(len(available)):
-        available[i] += allocation[index][i]
+def print_needed(n, m, needed_resources):
+    print("Needed Resources")
+    print("\tA\tB\tC")
+    for i in range(n):
+        print("P{i}".format(i=i), end="\t")
+        for j in range(m):
+            print(needed_resources[i][j], end='\t')
+        print()
+    print()
 
 
-def banker(maximum, allocation, needed, available):
-    index = 0
-    process = []
-    while(len(process) < len(maximum)):
-        if(index >= len(maximum)):
-            index = 0
-            while(index in process):
-                index += 1
-        if(compare(available, needed, allocation, index)):
-            addAvailable(available, allocation, index)
-            process.append(index)
-        index += 1
-    print("The safe sequence is :")
-    for i in range(len(process)):
-        print("P" + str(process[i]), end=" --> ")
+def banker(n, m, max_resources, allocated_resources):
+    needed_resources = calculate_needed(
+        n, m, max_resources, allocated_resources)
+
+    print_needed(n, m, needed_resources)
+
+    available = [0, 0, 2]
+    safe_sequence = []
+
+    flag = [0]*n
+    for i in range(n):
+        for j in range(n):
+            if flag[j] == 0:
+                curr_flag = True
+                for k in range(m):
+                    if needed_resources[j][k] > available[k]:
+                        curr_flag = False
+                        break
+
+                if curr_flag:
+                    for k in range(m):
+                        available[k] += allocated_resources[j][k]
+                    print("Currently Available Resources")
+                    print("A\tB\tC")
+                    print("\t".join(str(i) for i in available))
+                    flag[j] = 1
+                    safe_sequence.append(j)
+
+    print("Available Resources After Completion")
+    print("A\tB\tC")
+    print("\t".join(str(i) for i in available))
+    print()
+
+    if sum(flag) == len(flag):
+        print("Safe State")
+        print("Safe Sequence:")
+        for i in safe_sequence:
+            print("P{i}".format(i=i), "->", end=" ")
+    else:
+        print("Unsafe State")
+        for i in range(0, len(flag)):
+            if flag[i] == 0:
+                print("P{i}".format(i=i), "is incomplete")
 
 
-maximum = [[0, 0, 1, 2], [1, 7, 5, 0], [2, 3, 5, 6],
-           [0, 6, 5, 2], [0, 6, 5, 6]]
+num_processes = 3
+num_devices = 3
+max_resources = [[2, 2, 4], [2, 1, 3],
+                 [3, 4, 1]]
+allocated_resources = [[1, 2, 1], [2, 0, 1],
+                       [2, 3, 1]]
 
-allocation = [[0, 0, 1, 2], [1, 0, 0, 0], [1, 3, 5, 4],
-              [0, 6, 3, 2], [0, 0, 1, 4]]
-
-#needed = max - allocation
-needed = [[0]*len(maximum[0]) for i in range(len(maximum))]
-
-# if needed <= available then available + allocation = new available
-available = [1, 5, 2, 0]
-
-for i in range(0, len(allocation)):
-    findneeded(i, maximum, allocation, needed)
-
-banker(maximum, allocation, needed, available)
+banker(num_processes, num_devices, max_resources, allocated_resources)
